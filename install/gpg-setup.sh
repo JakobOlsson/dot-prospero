280fb3ee04bddc4c0e09cbe31324e89a43a812b2
JakobOlsson <32457895+JakobOlsson@users.noreply.github.com>
2020-06-11 19:13:55 +0200

    alt pubkey link

--- install/gpg-setup.sh
+++ install/gpg-setup.sh
@@ -2,3 +2,6 @@
 
 # import my public key from keybase
 curl https://keybase.io/magikern/pgp_keys.asc | gpg --import
+
+# Alternetive url
+# curl https://sylen.s3-eu-west-1.amazonaws.com/pub/pubkey.asc | gpg --import
