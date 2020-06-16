66a294f4a1d655682cb6ef06824b477fdaf61863
JakobOlsson <32457895+JakobOlsson@users.noreply.github.com>
2020-06-16 20:29:12 +0200

    fixed font install script

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 echo "Coping fonts"
-cp Inconsolata Bold Nerd Font Complete Mono.otf ~/.local/share/fonts
-cp Inconsolata Bold Nerd Font Complete.otf ~/.local/share/fonts
-cp Inconsolata Regular Nerd Font Complete Mono.otf ~/.local/share/fonts
-cp Inconsolata Regular Nerd Font Complete.otf ~/.local/share/fonts
+mkdir -p ~/.local/share/fonts
+cp "Inconsolata Bold Nerd Font Complete Mono.otf" ~/.local/share/fonts
+cp "Inconsolata Bold Nerd Font Complete.otf" ~/.local/share/fonts
+cp "Inconsolata Regular Nerd Font Complete Mono.otf" ~/.local/share/fonts
+cp "Inconsolata Regular Nerd Font Complete.otf" ~/.local/share/fonts
 echo "Clear an re-generate font cache"
 fc-cache -f -v
