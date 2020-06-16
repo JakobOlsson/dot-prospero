c9114017106ef26c61b42d3bded63ae05322ef1d
JakobOlsson <32457895+JakobOlsson@users.noreply.github.com>
2020-06-16 20:17:54 +0200

    update gpg script

--- install/gpg-setup.sh
+++ install/gpg-setup.sh
@@ -1,7 +1,10 @@
-#!/bin/user/env bash
+#!/usr/bin/env bash
 
 # import my public key from keybase
 curl https://keybase.io/magikern/pgp_keys.asc | gpg --import
 
 # Alternetive url
 # curl https://sylen.s3-eu-west-1.amazonaws.com/pub/pubkey.asc | gpg --import
+
+echo "Remember to edit and trust the key"
+echo "Also: If there is a built-in SmartCard-reader, it will conflict with USB based GNUPG SmartCard"
