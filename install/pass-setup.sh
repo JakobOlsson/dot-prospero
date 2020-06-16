ca6679f6c10288c396f3f7c20c6c01f2586bf306
Jakob Olsson <32457895+JakobOlsson@users.noreply.github.com>
2020-06-16 21:04:23 +0200

    pass setup helper for remote git

--- install/pass-setup.sh
+++ install/pass-setup.sh
@@ -1,2 +1,22 @@
-#!/bin/sh
-pass git clone keybase://private/magikern/pass
+#!/usr/bin/env bash
+
+echo "GnuPG key need to be imported"
+echo "init passwordstore"
+gpg2 --list-keys
+echo "Input key id from above:"
+read keyid
+pass init $keyid
+
+echo "setup pass git"
+pass git init
+
+echo "setup pass git remote"
+echo "give repo to clone:"
+read gitrepo
+pass git remote add origin $gitrepo
+pass git fetch --all
+pass git reset --hard origin/master
+pass git branch --set-upstream-to=origin/master master
+
+echo "getting pass from remote"
+pass git pull
