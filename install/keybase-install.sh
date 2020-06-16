54abaa4db92ef848504d8f22128892d4096a4234
JakobOlsson <32457895+JakobOlsson@users.noreply.github.com>
2020-06-16 20:34:19 +0200

    use dnf to install rpm. install keybase

---
+++ install/keybase-install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Getting keybase
+sudo dnf install https://prerelease.keybase.io/keybase_amd64.rpm
+# Run first setup
+run_keybase
--- install/vscode-setup.sh
+++ install/vscode-setup.sh
@@ -1,7 +1,8 @@
 # Install VSCode
 
 # Installing rpm
-rpm -ivh https://az764295.vo.msecnd.net/stable/5763d909d5f12fe19f215cbfdd29a91c0fa9208a/code-1.45.1-1589445456.el7.x86_64.rpm
+#rpm -ivh https://az764295.vo.msecnd.net/stable/5763d909d5f12fe19f215cbfdd29a91c0fa9208a/code-1.45.1-1589445456.el7.x86_64.rpm
+sudo dnf install https://az764295.vo.msecnd.net/stable/5763d909d5f12fe19f215cbfdd29a91c0fa9208a/code-1.45.1-1589445456.el7.x86_64.rpm
 
 # Installing extensions
 cat ../dotfiles/vscode_extensions.txt | xargs -L 1 code --install-extension
